d5d2378db2b0f657bcfd2a4b51964fbc3e8c9d16
psmruti794-ctrl <psmruti794@gmail.com>
2026-06-06 09:13:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+//alert( 3 + 3)//we r using nodejs,not browser
+
+console.log(3 + 3);  
+
+console.log("Smruti")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn =false
+let state = null
+   
+//DATATYPES(primitive types)
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standlone value(means empty/value is not asigned)
+// undifined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "hitesh")
+console.log(typeof null); // object
+
