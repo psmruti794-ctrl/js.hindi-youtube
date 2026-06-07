717c2819dc632351c8c5d3bb8f059e88bbc9a66c
psmruti794-ctrl <psmruti794@gmail.com>
2026-06-07 19:14:42 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,17 @@
+// Primitive
+
+// 7 types  : number, string, boolean, null, undefined, symbol, bigint
+v = 1; // number
+v = "hitesh"; // string
+v = true; // boolean
+v = null; // null
+v = undefined; // undefined
+
+// Non-primitive(reference type ):array, object, function
+v = [1, 2, 3]; // array
+v = { name: "hitesh", age: 23 }; // object
+v = function () {
+  console.log("hello");
+}; // function
+
+//*********** Type conversion**************//
