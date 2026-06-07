cc2089ea2e8c550960ee087f234eb6b7177817f8
psmruti794-ctrl <psmruti794@gmail.com>
2026-06-07 18:57:47 +0000

    conparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+//onsole.log(5 > 3); // true
+//console.log(5 < 3); // false
+//console.log(5 === 5); // true
+//console.log(5 !== 5); // false
+
+//console.log(5 >= 3); // true
+//console.log(5 <= 3); // false
+
+//console.log(null > 0); // false
+//console .log(null == 0); // false
+//console.log(null >= 0); // true
+
+//null is only equal to undefined and not equal to any other value
+
+//console.log(undefined > 0); // false
+//console.log(undefined == 0); // false
+//console.log(undefined >= 0); // false
+//console.log(undefined < 0); // true
+
+//===
+
+console .log(5 == "5"); // true
+console.log(5 === "5"); // false
